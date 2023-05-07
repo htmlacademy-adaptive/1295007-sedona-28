ad420a7f4695a38e51ae837d98589b41457ce124
alexaadm <admitrieva96@yandex.ru>
2023-05-07 17:38:27 +0500

    build ignored

--- build/js/map.js
+++
@@ -1,1 +0,0 @@
-const mapToggle=document.querySelector(".map__image-wrapper"),mapIframe=document.querySelector(".map__iframe");mapToggle.classList.remove("map__image-wrapper--no-js"),mapIframe.classList.remove("map__iframe--no-js");
