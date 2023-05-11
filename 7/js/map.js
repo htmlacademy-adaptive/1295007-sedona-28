937258357ee5f0bfe9aed3e378de4d6711c8b590
Keks <keks@htmlacademy.ru>
2023-05-11 12:59:43 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/map.js
@@ -0,0 +1,1 @@
+const mapToggle=document.querySelector(".map__image-wrapper"),mapIframe=document.querySelector(".map__iframe");mapToggle.classList.remove("map__image-wrapper--no-js"),mapIframe.classList.remove("map__iframe--no-js");
